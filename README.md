99afe92a6d485448c9b289cffdf6160a94ad58d6
JoshuaD <joshdadbin@Josh.local>
2023-03-18 09:44:01 -0700

    cleaned up code and added comments. Completerd README

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# dayScheduler
+# Day Scheduler
+
+https://autohome.github.io/dayScheduler/
+
+## Description
+
+This project was made to showcase javascript, local storage, and server side API skills. In this project, the user can enter in their schedule for the day. There are text areas for each time block, and when the save button is clicked, siad text will be saved to local storage. Upon refresh, text will reappear in correct time block. 
+
+Through dayjs API, the current day will be displayed at the top of the page. Also through dayjs, the color of the time blocks will change as the day the progresses. All future hours will be green, present will be red, and past will be grey.
+
+UCLA bootcamp provided started code for this project which included the base HTML and CSS.
+
+## Installation
+
+N/A
+
+## Usage
+
+The website address is [autohome.github.io/dayScheduler](https://autohome.github.io/dayScheduler/)
+
+To use the site, please enter a task in the text area of the time block and click save button on the far right of the same box.
+
+![Site image](./assets/images/sampleSchedule.png)
+
+## Credits
+
+Starter code provided was [coding-boot-camp by crispy-octo-meme](https://github.com/coding-boot-camp/crispy-octo-meme)
+
+Speciathanks to Shawn Tschoepe (UCLA bootcamp TA) for help implementing the .each throughout the JS code.
+
+Many thanks to UCLA bootcamp students and staff.
+
+## License
+
+MIT License
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,3 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 var saveButtons = $('.saveBtn');
 var textAreas = $('textarea');
 var timeRowEl = $('.time-block');  
@@ -12,27 +9,28 @@ var scheduledItems
 
 var inputEl 
 
-// add todays date
+// add todays date to page
 var date = today.format('dddd, MMMM D')
 $('#currentDay').text(date)
 
-  saveButtons.on('click', function(event) {
+// Pull text and save to local storage
+saveButtons.on('click', function(event) {
   event.stopPropagation();
   var text = $(this).siblings('textarea').val();
   var timeCode = $(this).parent().attr('id');
-  console.log(text);
-  console.log(timeCode)
   // need to store in local storage (JSON)
   localStorage.setItem(timeCode, JSON.stringify(text));
 });
 
-
+// change color of timeblock by current hour
 function checkTime() {
+  // what is current hour
   var currentHour = dayjs().hour();
-  
+  // run function for each time-block
   $('#container').children().each(function() {
+    // makes current hour match time block id (eg. hour-9)
     var hour = parseInt($(this).attr('id').split('-')[1]);
-
+    // if then statement to add correct class to timeblock
     if (hour < currentHour) {
       $(this).addClass('past');
     } else if (hour > currentHour) {
@@ -43,29 +41,26 @@ function checkTime() {
   });
 }
 
-  function init() {
-    checkTime()
+// initialise function to pull storage data and display on screen
+function init() {
+  // run checktime to fill in colors
+  checkTime()
+    // for loop to have each time block id
     for (var i = 9; i <=17; i++) {
       var timeCode = "hour-" + i;
+      // pulls each timeblocl text out of storage
       var storedText = JSON.parse(localStorage.getItem(timeCode));
-      // console.log(timeCode)
-      
-      if (storedText !== null) {
-
-        for (var x = 0; x < $("#container").children().length; x++)
-
-        // console.log(timeCode)
-        // console.log($("#container").children().eq(x).attr("id"))
-        if (timeCode === $("#container").children().eq(x).attr("id")) {
-          // console.log(timeCode + x + storedText + $("#container").children().eq(x).children("textarea"))
-          var insertText = $("#container").children().eq(x).children("textarea");
-          // console.log(insertText + storedText);
-          insertText.val(storedText);
-          
-        }
-      } 
+        // if text is not blank, add to correct time block
+        if (storedText !== null) {
+          for (var x = 0; x < $("#container").children().length; x++)
+            // make sure time block matches storage ID
+            if (timeCode === $("#container").children().eq(x).attr("id")) {
+              var insertText = $("#container").children().eq(x).children("textarea");
+              insertText.val(storedText);
+            }
+        } 
     }
-  }
+}
 
-
+// run initial function on startup
 init ();
